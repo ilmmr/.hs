f93f7a43e5fab7ec25efd4d2b5a4ff0160aaff85
ilmmr <lmmr_@outlook.pt>
2024-08-19 23:40:12 +0100

    Back to .hs problems ¯\_(ツ)_/¯

--- Problems/TheCultiveProblem.hs
+++ Problems/TheCultiveProblem.hs
@@ -5,6 +5,7 @@ import Data.Tuple
 
 type Matriz a = [[a]]
 type Position = (Int, Int)
+data Tree a = Node a [Tree a] deriving (Show, Eq)
 
 split :: (a -> b) -> (a -> c) -> a -> (b,c)
 split f g x = (f x, g x)
@@ -13,17 +14,23 @@ split f g x = (f x, g x)
 f >< g = split (f . fst) (g . snd)
 
 matrix_size :: Matriz Int -> (Int, Int)
-matrix_size = split (length . head) length
+matrix_size = split length (length . head)
 
-around :: Position -> (Int, Int) -> [Position]
-around (x,y) (n, m) = filter ( f (n, m)) . concat $ [
-  return (x-1,y),
+-- Generate a tree from a given position in the matrix
+generate_tree :: Matriz Int -> Position -> Tree Position
+generate_tree m pos = Node pos (map (generate_tree m) children)
+  where
+    size      = matrix_size m
+    children  = around_front pos size
+
+around_front :: Position -> (Int, Int) -> [Position]
+around_front (x,y) (n, m) = filter ( f (n, m)) . concat $ [
   return (x+1,y),
-  return (x,y+1),
-  return (x,y-1)] where f (n, m) = (\p -> fst p >= 0 && snd p >= 0 && fst p < n && snd p < m)
+  return (x,y+1)
+  ] where f (n, m) = (\p -> fst p >= 0 && snd p >= 0 && fst p < n && snd p < m)
 
 position_calculation :: Matriz Int -> Position -> Int
-position_calculation m = uncurry (*) . split (index m) ((.) ((.) (sum) . map . index $ m) . flip around . matrix_size $ m)
+position_calculation m = uncurry (*) . split (index m) ((.) ((.) (sum) . map . index $ m) . flip around_front . matrix_size $ m)
 {-
   position_calculation \m pos = (index m pos) * (sum . map (index m) $ (around pos (matrix_size m)))
   position_calculation \m pos = (*) (index m pos) (sum . map (index m) $ around pos (matrix_size m))
@@ -49,26 +56,21 @@ index = (uncurry (!!) .) . flip split fst . flip (.) snd . (!!)
   index        = (uncurry (!!) .) . flip split snd . flip (.) fst . (!!)
 -}
 
-calculate :: Matriz Int -> Matriz Int
-calculate m = [[(position_calculation m (x, y)) | x <- [0..rows-1]] | y <- [0..cols-1]]
-  where
-    rows = snd . matrix_size $ m
-    cols = fst . matrix_size $ m
-
 build :: Matriz Char -> Matriz Int
 build = map . map $ convert 
     where   convert '#' = 0
             convert '.' = 1
 
-{- Input: Matriz [['#', '.'] ...], where '#' means occupied and '.' means free. -}
--- main :: Matriz Char -> ([Position], Int) 
--- main = uncurry (><) (id, length) . f . calculate . build where f n = (,) n n
 {- 
+Input: Matriz [['#', '.'] ...], where '#' means occupied and '.' means free.
+main :: Matriz Char -> ([Position], Int) 
+main = uncurry (><) (id, length) . f . calculate . build where f n = (,) n n 
     main \m = (><) id length (f (calculate (build m))) where f n = (,) n n
     main \m = (><) id length (f (calculate (build m))) where f n = (,) n n
     main \m = (><) id length $ f $ calculate . build $ m where f n = (,) n n
     main    = (><) id length . f . calculate . build where f n = (,) n n 
 -}
 
-tmp :: Matriz Char -> Matriz Int
-tmp = calculate . build
+tmp :: Matriz Char -> Tree Position
+tmp = flip generate_tree initial . build
+  where initial = (0,0)
